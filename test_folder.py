133be150747a1f2c2f6ab206f06b7f7a8ccf128a
LungHuiWu <45966719+LungHuiWu@users.noreply.github.com>
2022-01-05 22:58:02 +0800

    Update test_folder.py

--- test_folder.py
+++ test_folder.py
@@ -130,7 +130,7 @@ load_checkpoint(args.model, model, optimizer)
 
 t2i = transforms.Compose([
     #transforms.Resize(256),
-    transforms.CenterCrop(256),
+    #transforms.CenterCrop(256),
     transforms.ToTensor(),
     #transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225]),
 ])
