6307ec6a6e74c6ebf7700becc9e2fd679463861b
LungHuiWu <45966719+LungHuiWu@users.noreply.github.com>
2022-01-06 12:01:14 +0800

    Update denoise.sh

--- denoise.sh
+++ denoise.sh
@@ -1,3 +1,3 @@
 
-wget -O model.pth https://www.dropbox.com/s/7yryykmzk06snvd/DenoiseAE_UNet_loss.pth?dl=1
+wget -O model.pth https://www.dropbox.com/s/4j7os8krnjmynbn/Deblur_UNet.pth?dl=1
 python3 test.py --inimage $1 --outimage $2 --model model.pth
